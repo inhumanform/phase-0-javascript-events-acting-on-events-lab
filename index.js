bf15e005fbe7968241bf5d26385452dde686d246
Drew <dch528@gmail.com>
2024-01-21 17:05:30 -0500

    Drew Hairston JS Events Acting Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+   ;
+  const dodger = document.getElementById("dodger");
+  
+  
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    const maxRight = window.innerWidth - dodger.offsetWidth;
+    if (left < maxRight) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
